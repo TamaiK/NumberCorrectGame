a16c1d494b001569e6033a316d7ec027c487ab1f
TamaiK <tamaikohsuke1118@gmail.com>
2021-10-26 15:49:15 +0900

    前置き表示と答えの生成を関数化

--- src/App.java
+++ src/App.java
@@ -3,33 +3,45 @@ import java.util.Scanner;
 
 public class App {
 
+    // 乱数生成クラス
     private static Random random;
+    // 入力取得に使うクラス
+    private static Scanner scanner;
+
+    // 定数
     // 挑戦可能回数
     private static int MAX_CHALLENG = 5;
-    // 乱数の最大値
+    // 乱数の下限値
+    private static int MIN_VALUE = 0;
+    // 乱数の上限値
     private static int MAX_VALUE = 99;
     // 入力許容回数
     private static int MAX_INPUT_LIMIT = 10;
+
+    // 定数（文字）
+    // 改行用
+    private static String MESSAGE_FOR_BLANK = "";
+    // 前置き
+    private static String MESSAGE_FOR_PREFACE = "数を当ててみてね。";
+    // 挑戦上限
+    private static String MESSAGE_FORMAT_FOR_LIMIT_COUNT = "答えられるのは %d 回までだよ。";
+    
+    // メイン関数
     public static void main(String[] args) throws Exception {
-        // 初期化
-        random = new Random();
-        // 読み取りクラス
-        Scanner scanner = new Scanner(System.in);
+
+        Init();
+
+        // 前置きの表示
+        DispPreface();
 
         // 各変数の用意
         // 挑戦回数
         int challengeCount = 0;
         // 正解の値
-        // 引数の値-1までの範囲で乱数を作るので+1する
-        int correctNum = random.nextInt(MAX_VALUE+1);
+        int correctNum = CreateRandomNumber(MIN_VALUE, MAX_VALUE);
 
         // 回答用変数
         int answerNum = 0;
-
-        System.out.println("数を当ててみてね。");
-        System.out.println("");
-        System.out.println(String.format("答えられるのは %d 回までだよ。", MAX_CHALLENG));
-        System.out.println("");
 
         // 挑戦回数以内か
         // 実質セーフティ
@@ -110,7 +122,45 @@ public class App {
             }
         }
 
-        // 最後に閉じる
+        Fin();
+    }
+
+    // 初期化関数
+    private static void Init() {
+
+        // 最初に処理が必要なものをここに入れる
+
+        // 乱数
+        random = new Random();
+        // 読み取りクラス
+        scanner = new Scanner(System.in);
+    }
+
+    // 終了関数
+    private static void Fin() {
+
+        // 最後に処理が必要なものをここに入れる
+        
+        // 閉じておく
         scanner.close();
+    }
+
+    // 前置きの表示
+    private static void DispPreface() {
+
+        System.out.println(MESSAGE_FOR_PREFACE);
+        System.out.println(MESSAGE_FOR_BLANK);
+        System.out.println(String.format(MESSAGE_FORMAT_FOR_LIMIT_COUNT, MAX_CHALLENG));
+        System.out.println(MESSAGE_FOR_BLANK);
+    }
+
+    // 範囲内で無作為な値を作成する関数
+    private static int CreateRandomNumber(int min, int max) {
+
+        // 差だけでは1足りなくなる（元の値の分がカウントされない）ので＋１
+        int range = max - min + 1;
+
+        // 指定の大きさから乱数を作り、最小値を足すことで指定範囲の乱数にする
+        return random.nextInt(range) + min;
     }
 }
